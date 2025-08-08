2eef7036c7d8b7d90a187ca4d048199d02fedf8a
pathan2303 <shabazshaikh@hotmail.com>
2025-08-08 20:52:19 +0530

    Add proxy server for CORS fix

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const request = require('request');
+const app = express();
+
+app.use(express.json());
+
+app.all('/proxy', (req, res) => {
+  const url = 'https://script.google.com/macros/s/AKfycbxVhtPq8fvdCjRR00gf-EsyNAyI9ZQoHWqCrMiaXmJsliqL2o7j75A-AS3ij34Qsk0G/exec'; // Replace with your URL
+  req.pipe(request({ url, method: req.method, json: req.body })).pipe(res);
+});
+
+// Add a simple test route
+app.get('/', (req, res) => {
+  res.send('Proxy Server is Running!');
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log('Proxy server running on port 3000');
+});
