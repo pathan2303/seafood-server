ebc07a36acbec3ea37bb3dbb3a3e25d383fca9ea
pathan2303 <shabazshaikh@hotmail.com>
2025-08-10 15:49:42 +0530

    Add CORS support to proxy server

--- server.js
+++ server.js
@@ -1,17 +1,14 @@
 const express = require('express');
 const request = require('request');
+const cors = require('cors');
 const app = express();
 
 app.use(express.json());
+app.use(cors()); // Enable CORS for all routes
 
 app.all('/proxy', (req, res) => {
-  const url = 'https://script.google.com/macros/s/AKfycbxVhtPq8fvdCjRR00gf-EsyNAyI9ZQoHWqCrMiaXmJsliqL2o7j75A-AS3ij34Qsk0G/exec'; // Replace with your URL
+  const url = 'https://script.google.com/macros/s/AKfycbxVhtPq8fvdCjRR00gf-EsyNAyI9ZQoHWqCrMiaXmJsliqL2o7j75A-AS3ij34Qsk0G/exec'; // Replace with your Apps Script URL
   req.pipe(request({ url, method: req.method, json: req.body })).pipe(res);
-});
-
-// Add a simple test route
-app.get('/', (req, res) => {
-  res.send('Proxy Server is Running!');
 });
 
 app.listen(process.env.PORT || 3000, () => {
